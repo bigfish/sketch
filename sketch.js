cca156ed63ad7d9d63df6baad5da5873d3522a87
David Wilhelm <dewilhelm@gmail.com>
2010-08-28 19:14:58 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,99 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var canvas_x, canvas_y;
+var canvas_pos;
+var line_start;
+var drawing = false;
+var path = [];//array points
+//var mode = "linear";//draw points whenever mouse moves
+var mode = "continuous";//draw points whenever mouse moves
+
+function getElementPosition(obj) {
+    var curleft = 0,
+    curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+            obj = obj.offsetParent;
+        } while (obj);
+    }
+    return { x: curleft, y: curtop};
+}
+
+function clearScreen() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function startLine(x, y) {
+    drawing = true;
+    ctx.beginPath();
+    line_start = {x: x, y: y};
+    ctx.moveTo(x, y);
+}
+
+function drawPath() {
+    clearScreen();
+    if (path.length === 0) {
+        return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(path[0].x, path[0].y);
+    for (var p = 0; p < path.length; p++) {
+        ctx.lineTo(path[p].x, path[p].y);
+    }
+    ctx.stroke();
+}
+
+function drawLine(x, y) {
+    if(mode == "continuous"){
+        //add a line segment to the current point
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        path.push({x: x, y: y});
+
+    } else if(mode == "linear"){
+        //show where the next line will be drawn on mouse up
+        //redraw existing lines
+        clearScreen();
+        ctx.beginPath();
+        drawPath();
+        //draw last line to current point
+        ctx.lineTo(x, y);
+        ctx.stroke();
+
+    }
+}
+
+
+function endLine(x, y) {
+    drawLine(x, y);
+    //point will added in drawLine if mode is continuous
+    if (mode=="linear") {
+       path.push({x:x,y:y});
+    } 
+    drawing = false;
+}
+
+function onMouseDown(e) {
+    startLine(e.offsetX, e.offsetY);
+}
+
+function onMouseUp(e) {
+    endLine(e.offsetX, e.offsetY);
+}
+
+function onMouseMove(e) {
+    if (drawing) {
+        drawLine(e.offsetX, e.offsetY);
+    }
+
+}
+
+//MAIN
+canvas_pos = getElementPosition(canvas);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mousemove", onMouseMove);
+ctx.strokeStyle = "#000000";
+ctx.lineWidth=0;
